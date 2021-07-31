b6641fcbc8ab45195b4d22dab367afde63bd51f8
Beatriz <beajardim@gmail.com>
2021-07-31 19:47:43 +0100

    atualização das categorias

--- categorias.php
+++ categorias.php
@@ -1,7 +1,17 @@
-<div class="cx12"> 
-    <div class="subcx1"></div>
-    <div class="subcx2"></div>
+<div class="cx1">
+    <div class="subcx1">
+        <p>Roupa</p>
+    </div>
 </div>
+   
+<div class="cx2">
+    <div class="subcx2">
+        <p>Acessórios</p>
+    </div>
+</div>
+
 <div class="cx3"> 
-    <div class="subcx3"></div>
+    <div class="subcx3">
+        <p>Amigurumis</p>
+    </div>
 </div>
--- slider.php
+++ slider.php
@@ -1,7 +1,11 @@
 <div class="novidades1"> 
     <h1>Nova Coleção</h1>
     <h2>Verão 2021 </h2>
-    <p>Roupa</p>
-    <p>Acessórios</p>
-    <p>Amigurumis</p>
+        <ul>
+            <li>Roupa</li>
+            <li><i class="fas fa-grip-lines-vertical"></i></li>
+            <li>Acessórios</li>
+            <li><i class="fas fa-grip-lines-vertical"></i></li>
+            <li>Amigurumis</li>
+        </ul>
 </div>
