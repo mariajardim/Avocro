1ce2f2bd40c4689d2e4b1484e903552dce97b2f5
Beatriz <beajardim@gmail.com>
2021-07-31 22:37:35 +0100

    alterações desconto

--- desconto.php
+++ desconto.php
@@ -1,2 +1,2 @@
-<p> <b>10% de desconto</b> na primeira encomenda</p>
-<button type="button" class="btn btn-primary">#ILOVEAVOCRO</button>
+    <p> <b>10% de desconto</b> na primeira encomenda</p>
+    <button type="button" class="btn btn-primary">#ILOVEAVOCRO</button>
