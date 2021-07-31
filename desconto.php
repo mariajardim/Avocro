35f08da925b69d7304f2540e26037b560ff54af5
Beatriz <beajardim@gmail.com>
2021-07-31 23:51:36 +0100

    destaques

--- desconto.php
+++ desconto.php
@@ -1,2 +1,7 @@
-    <p> <b>10% de desconto</b> na primeira encomenda</p>
+<div class="desconto">
+     <p> <b>10% de desconto</b> na primeira encomenda</p>
+</div>
+
+<div class="areabotao">
     <button type="button" class="btn btn-primary">#ILOVEAVOCRO</button>
+</div>
--- destaques.php
+++ destaques.php
@@ -1,12 +1,26 @@
-<div class="titulo"></div>
-<ul>
-    <li>A</li>
-    <li>B</li>
-    <li>C</li>
-    <li>D</li>
-    <li>E</li>
-    <li>F</li>
-    <li>G</li>
-    <li>H</li>
-    <li>I</li>
-</ul>
+<div class="titulo">
+    <p>Destaques</p>
+</div>
+
+<div class="filtros">
+    <ul>
+            <li>Ver Todos</li>
+            <li>Roupa</li>
+            <li>Acessórios</li>
+            <li>Amigurumis</li>
+    </ul>
+</div>
+
+<div class="produtos">
+    <ul>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+        <li>F</li>
+        <li>G</li>
+        <li>H</li>
+        <li>I</li>
+    </ul>
+</div>
