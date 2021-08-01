3480455bc752f22f6d9bad599acff63443777c87
Beatriz <beajardim@gmail.com>
2021-08-01 19:08:42 +0100

    footer ok

---
+++ footer.php
@@ -0,0 +1,42 @@
+<div class="parte1">
+    <h3>NOME DA EMPRESA </h3>
+    <ul>
+        <li>Quem Somos? </li>
+    </ul>
+</div>
+
+<div class="parte2">
+    <h3>LEGALIDADES </h3>
+    <ul>
+        <li>Política de Privacidade </li>
+        <li>Política de Cookies </li>
+    </ul>
+</div>
+
+<div class="parte3">
+    <h3>Termos e Condições </h3>
+    <ul>
+        <li>Reclamações </li>
+    </ul>
+</div>
+
+<div class="parte4">
+    <h3>DÚVIDAS </h3>
+    <ul>
+        <li>FAQS </li>
+        <li>Fala connosco!</li>
+    </ul>
+</div>
+
+<div class="parte5">
+    <h3>CONTACTOS</h3>
+    <ul>
+        <li><i class="fas fa-envelope-open-text"></i></li>
+        <li><i class="fab fa-instagram"></i></li>
+        <li><i class="fab fa-facebook-f"></i></li>
+    </ul>
+</div>
+
+<div class="parte6">
+    <p>© 2021 Portugal</p>
+</div>
