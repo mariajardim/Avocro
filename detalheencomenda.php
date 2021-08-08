f731c98a0da1542571ed4933f00f0aac4e260dc2
Beatriz <beajardim@gmail.com>
2021-08-08 21:23:47 +0100

    estrutura detalhe da encomenda

--- detalheencomenda.php
+++ detalheencomenda.php
@@ -1,1 +1,75 @@
-ola detalhe encomenda
+<div class="detalheencomenda1">
+    <div class="detencomenda">
+            <h2>Encomenda Nº 0003</h2>
+    </div>
+    <div class="encomenda-body">
+            <div class="info-encomenda">
+                <p>Estado da Encomenda: Em Preparação</p>
+                <p>Dia da Encomenda: 12/08/2021</p>
+                <p>Previsão de Entrega: 12/09/2021</p>
+            </div>
+            <div class="info-moradaenvio">
+                <h3>Morada de Envio</h3>
+                <p>Avenida das forças armadas n1, 1600-078 Lisboa</p>
+                
+            </div>
+            <div class="info-moradafat">
+                <h3>Morada de faturação</h3>
+                <p>Avenida das forças armadas n1, 1600-078 Lisboa</p>
+              
+            </div> 
+            <div class="detalhe-produtos-encomenda">
+                <h3 style="margin-bottom:0px;">Detalhe</h3>
+                <div class="titulotabela">
+                    <ul>
+                        <li>
+                            <h3 style="margin-bottom:0px;">Valor Unitário</h3>
+                        </li>
+                        <li>
+                            <h3>Unidades</h3>
+                        </li>
+                        <li>
+                            <h3>Valor Total</h3>
+                        </li>
+                    </ul>
+                </div>
+                <div class="tabela-produtos">
+                    <ul>
+                        <li>imagem</li>
+                        <li>detalhe dos produtos</li>
+                        <li>Valor Unitário</li>
+                        <li>Unidades</li>
+                        <li>Valor Total</li>
+                    </ul>
+                    <ul>
+                        <li>imagem</li>
+                        <li>detalhe dos produtos</li>
+                        <li>Valor Unitário</li>
+                        <li>Unidades</li>
+                        <li>Valor Total</li>
+                    </ul>
+                </div>
+            </div> 
+            <div class="total-encomenda">
+                <div class="descricao-totais">
+                    <ul>
+                        <li>Total Encomenda</li>
+                        <li>Custo Envio</li>
+                        <li>Descontos</li>
+                        <li>Valor Total</li>
+                    </ul>
+                </div>
+                <div class="valores-totais">
+                    <ul>
+                        <li>Valor</li>
+                        <li>Valor</li>
+                        <li>Valor</li>
+                        <li>Valor</li>
+                    </ul>
+                </div>
+            </div> 
+    </div>
+</div>
+
+
+    
