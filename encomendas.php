8fc067b4fafc943eea56bf2c10b6deca10334d53
Beatriz <beajardim@gmail.com>
2021-08-08 20:21:51 +0100

    pagina encomendas colorida

--- encomendas.php
+++ encomendas.php
@@ -1,10 +1,30 @@
 <div class="encomendas1">
         <ul>
             <li>
-                <div class="underline"></div>
+            <div class="underline"></div>
                 <a href="minhaconta.php?area=detalheencomenda">
                     <div class="box">   
-                        <div class="box1"></div>
+                        <div class="box1">
+                            <div class="sub-box1"></div>
+                        </div>
+                        <div class="box2">
+                            <div class="sub-box2">
+                                <p>Encomenda Nº 0003</p>
+                                <p>A Preparar</p>
+                                <p>12/08/2021 - 10,00 €</p>
+                            </div>
+                        </div>
+                        <div class="box3"><i class="fas fa-eye"></i></div>
+                    </div>  
+                </a>
+            </li>
+            <li>
+            <div class="underline"></div>
+                <a href="minhaconta.php?area=detalheencomenda">
+                    <div class="box">   
+                        <div class="box1">
+                            <div class="sub-box1"></div>
+                        </div>
                         <div class="box2">
                             <div class="sub-box">
                                 <p>Encomenda Nº 0003</p>
@@ -17,10 +37,12 @@
                 </a>
             </li>
             <li>
-                <div class="underline"></div>
+            <div class="underline"></div>
                 <a href="minhaconta.php?area=detalheencomenda">
                     <div class="box">   
-                        <div class="box1"></div>
+                        <div class="box1">
+                            <div class="sub-box1"></div>
+                        </div>
                         <div class="box2">
                             <div class="sub-box">
                                 <p>Encomenda Nº 0003</p>
@@ -33,26 +55,12 @@
                 </a>
             </li>
             <li>
-                <div class="underline"></div>
+            <div class="underline"></div>
                 <a href="minhaconta.php?area=detalheencomenda">
                     <div class="box">   
-                        <div class="box1"></div>
-                        <div class="box2">
-                            <div class="sub-box">
-                                <p>Encomenda Nº 0003</p>
-                                <p>A Preparar</p>
-                                <p>12/08/2021 - 10,00 €</p>
-                            </div>
+                        <div class="box1">
+                            <div class="sub-box1"></div>
                         </div>
-                        <div class="box3"><i class="fas fa-eye"></i></div>
-                    </div>  
-                </a>
-            </li>
-            <li>
-                <div class="underline"></div>
-                <a href="minhaconta.php?area=detalheencomenda">
-                    <div class="box">   
-                        <div class="box1"></div>
                         <div class="box2">
                             <div class="sub-box">
                                 <p>Encomenda Nº 0003</p>
