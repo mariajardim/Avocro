e5be35e4cde8866e1958cd1eb110406d1a3aeb5d
Beatriz <beajardim@gmail.com>
2021-08-08 23:10:15 +0100

    detalhe da encomenda completa

--- detalheencomenda.php
+++ detalheencomenda.php
@@ -26,45 +26,73 @@
                             <h3 style="margin-bottom:0px;">Valor Unitário</h3>
                         </li>
                         <li>
-                            <h3>Unidades</h3>
+                            <h3 style="margin-bottom:0px;">Unidades</h3>
                         </li>
                         <li>
-                            <h3>Valor Total</h3>
+                            <h3 style="margin-bottom:0px;">Valor Total</h3>
                         </li>
                     </ul>
                 </div>
                 <div class="tabela-produtos">
+                    <div class="underline"></div>
                     <ul>
-                        <li>imagem</li>
-                        <li>detalhe dos produtos</li>
-                        <li>Valor Unitário</li>
-                        <li>Unidades</li>
-                        <li>Valor Total</li>
+                        <li style="padding-right: 10px;">
+                            <div class="img"></div>
+                        </li>
+                        <li style="width: 33.33%; flex-direction: column; margin-bottom: 20px;">
+                                <p style="justify-content: flex-start; padding-left: 10px; padding-bottom:10px;">Amigurumi Mostrinho</p>
+                                <p style="justify-content: flex-start; padding-left: 10px; padding-bottom:10px;">Ref.124-2021</p>
+                                <p style="justify-content: flex-start; padding-left: 10px; padding-bottom:10px;">Tamanho: Pequeno</p>
+                                <p style="justify-content: flex-start; padding-left: 10px; padding-bottom:10px;">Cor: Azul</p>
+                        </li>
+                        <li>
+                            <p>5,00€</p>
+                        </li>
+                        <li>
+                            <p>1</p>
+                        </li>
+                        <li>
+                            <p>5,00€</p>
+                        </li>
                     </ul>
                     <ul>
-                        <li>imagem</li>
-                        <li>detalhe dos produtos</li>
-                        <li>Valor Unitário</li>
-                        <li>Unidades</li>
-                        <li>Valor Total</li>
+                        <li style="padding-right: 10px;">
+                            <div class="img"></div>
+                        </li>
+                        <li style="width: 33.33%; flex-direction: column;  margin-bottom: 20px;">
+                                <p style="justify-content: flex-start; padding-left: 10px; padding-bottom:10px;">Amigurumi Mostrinho</p>
+                                <p style="justify-content: flex-start; padding-left: 10px; padding-bottom:10px;">Ref.124-2021</p>
+                                <p style="justify-content: flex-start; padding-left: 10px; padding-bottom:10px;">Tamanho: Pequeno</p>
+                                <p style="justify-content: flex-start; padding-left: 10px; padding-bottom:10px;">Cor: Azul</p>
+                        </li>
+                        <li>
+                            <p>3,00€</p>
+                        </li>
+                        <li>
+                            <p>1</p>
+                        </li>
+                        <li>
+                            <p>3,00€</p>
+                        </li>
                     </ul>
+                    <div class="underline"></div>
                 </div>
             </div> 
             <div class="total-encomenda">
                 <div class="descricao-totais">
-                    <ul>
-                        <li>Total Encomenda</li>
-                        <li>Custo Envio</li>
-                        <li>Descontos</li>
-                        <li>Valor Total</li>
+                    <ul style="flex-direction: column;align-items: flex-end;margin: 0px;padding: 0px;">
+                        <li style="padding: 0px;margin: 0px;">Total Encomenda</li>
+                        <li style="padding: 0px;margin: 0px;">Custo Envio</li>
+                        <li style="padding: 0px;margin: 0px;">Descontos</li>
+                        <li style="padding: 0px;margin: 0px;">Total (IVA incluído)</li>
                     </ul>
                 </div>
                 <div class="valores-totais">
-                    <ul>
-                        <li>Valor</li>
-                        <li>Valor</li>
-                        <li>Valor</li>
-                        <li>Valor</li>
+                    <ul style="flex-direction: column;align-items: flex-end;margin: 0px;padding: 0px;">
+                        <li style="padding: 0px;margin: 0px;">8,00€</li>
+                        <li style="padding: 0px;margin: 0px;">5,00€</li>
+                        <li style="padding: 0px;margin: 0px;">-5,00€</li>
+                        <li style="padding: 0px;margin: 0px;">8,00€</li>
                     </ul>
                 </div>
             </div> 
