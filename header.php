73497edea6d482486e37266659b2fa54f52eb11f
Beatriz <beajardim@gmail.com>
2021-08-09 02:26:21 +0100

    mobile homepage

--- header.php
+++ header.php
@@ -19,7 +19,22 @@
             <div id="menu">
                 <?php include('menu.php'); ?>
             </div>
-
+        </li>
+    </ul>
+ </div>
+ <div class="menufixo">
+    <ul>
+        <li> 
+            <a href="index.php?"><i class="fas fa-home"></i></a>
+        </li>
+        <li> 
+            <a href="pesquisa.php?"><i class="fas fa-search"></i></a>
+        </li>
+        <li> 
+            <a href="wishlist.php?"><i class="fas fa-heart"></i></a>
+        </li>
+        <li> 
+            <a href="login.php?"><i class="fas fa-user-alt"></i></a>
         </li>
     </ul>
  </div>
